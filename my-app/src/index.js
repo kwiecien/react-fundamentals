9b543496f60afb3e4285dbf2d7c0d08baf4a1c24
kwiecien <k.d.kwiecinski@gmail.com>
2018-10-26 00:17:03 +0200

    feat: Add click counting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let model = {
+  clicks: 0
+};
+function render() {
+  ReactDOM.render(
+    <App
+      clicks={model.clicks}
+      onClick={() => {
+        model.clicks += 1;
+        render();
+      }}
+    />,
+    document.getElementById("root")
+  );
+}
+render();
+serviceWorker.register();
