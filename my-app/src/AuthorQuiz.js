11a38afc95ff136be78f455f222b95785e6e58ef
kwiecien <k.d.kwiecinski@gmail.com>
2018-10-27 23:10:31 +0200

    feat: Bootstrap the AuthorQuiz

---
+++ my-app/src/AuthorQuiz.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./bootstrap.min.css";
+
+class AuthorQuiz extends Component {
+  render() {
+    return <div>AuthorQuiz</div>;
+  }
+}
+
+export default AuthorQuiz;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import AuthorQuiz from "./AuthorQuiz";
 import * as serviceWorker from "./serviceWorker";
 
 let model = {
   clicks: 0
 };
 function render() {
-  ReactDOM.render(
-    <App
-      clicks={model.clicks}
-      onClick={() => {
-        model.clicks += 1;
-        render();
-      }}
-    />,
-    document.getElementById("root")
-  );
+  ReactDOM.render(<AuthorQuiz />, document.getElementById("root"));
 }
 render();
 serviceWorker.register();
