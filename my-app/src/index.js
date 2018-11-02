d190f1dab489cf0f841210b052891e07732fae2e
kwiecien <k.d.kwiecinski@gmail.com>
2018-11-02 23:15:19 +0100

    feat: Add onClick handlers

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -59,8 +59,21 @@ function getTurnData(authors) {
 }
 
 const state = {
-  turnData: getTurnData(authors)
+  turnData: getTurnData(authors),
+  highlight: "correct"
 };
 
-ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some(book => book === answer);
+  state.highlight = isCorrect ? "correct" : "wrong";
+  render();
+}
+
+function render() {
+  ReactDOM.render(
+    <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />,
+    document.getElementById("root")
+  );
+}
+render();
 serviceWorker.register();
